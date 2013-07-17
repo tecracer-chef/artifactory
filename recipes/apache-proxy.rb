3f7a4256fe7219fb0d6ab62a5b051b77e6b5d371
Miles Burton <miles@mnetcs.com>
2013-07-17 14:12:02 +0100

    Update apache-proxy.rb

--- recipes/apache-proxy.rb
+++ recipes/apache-proxy.rb
@@ -3,8 +3,17 @@ include_recipe "apache2"
 
 template "#{node['apache']['dir']}/sites-available/artifactory" do
 	source "apache-artifactory-vhost.conf.erb"
-	mode "0644"
-	notifies :reload, "service[apache2]"
+	owner       'root'
+	group       'root'
+	mode        '0644'
+	variables(
+	:host_name        => host_name,
+	:jenkins_port     => node['artifactory']['port']
+	)
+	
+	if File.exists?("#{node['apache']['dir']}/sites-enabled/artifactory")
+		notifies  :restart, 'service[apache2]'
+	end
 end
 
 apache_site "artifactory" do
