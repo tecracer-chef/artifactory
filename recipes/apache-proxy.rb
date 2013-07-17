b4ab46523f3d5244bace288cb00d715c88a11896
Miles Burton <miles@mnetcs.com>
2013-07-17 13:55:16 +0100

    Update apache-proxy.rb

--- recipes/apache-proxy.rb
+++ recipes/apache-proxy.rb
@@ -1,7 +1,7 @@
 include_recipe "artifactory"
 include_recipe "apache2::mod_proxy_http"
 
-template ::File.join(node["apache"]["dir"], "sites-available", "artifactory") do
+template "#{node['apache']['dir']}/sites-available/artifactory" do
 	source "apache-artifactory-vhost.conf.erb"
 	mode "0644"
 	notifies :reload, "service[apache2]"
