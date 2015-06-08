c938f3f37b1a47784eea7068e555653982907f43
Ronald Gundlach-Chmara <chmarar@webtrends.com>
2015-06-08 23:01:36 +0000

    Update for latest artifactory version and add ajp as a connection option.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default['artifactory']['zip_url'] = 'http://dl.bintray.com/content/jfrog/artifactory/artifactory-3.4.1.zip?direct'
-default['artifactory']['zip_checksum'] = '5019e4a4cac7936b3d4e1fc457d36fff60cdf27de42886184b0b5a844f43f0b0'
+default['artifactory']['zip_url'] = 'http://dl.bintray.com/content/jfrog/artifactory/artifactory-3.8.0'
+default['artifactory']['zip_checksum'] = '8f5e2b46bb5db96afd2185cd74edd5ec87a5ee5e7d7d3ec86500e44730917c69'
 default['artifactory']['home'] = '/var/lib/artifactory'
 default['artifactory']['log_dir'] = '/var/log/artifactory'
 default['artifactory']['catalina_base'] = ::File.join(artifactory['home'], 'tomcat')
@@ -11,3 +11,10 @@ default['artifactory']['user'] = 'artifactory'
 default['artifactory']['port'] = 8081
 default['artifactory']['shutdown_port'] = 8015
 default['artifactory']['install_java'] = true
+
+default['artifactory']['use_ajp'] = false
+default['artifactory']['ajp']['port'] = 8019
+default['artifactory']['ajp']['maxThreads'] = 500
+default['artifactory']['ajp']['minSpareThreads'] = 20
+default['artifactory']['ajp']['enableLookups'] = false
+default['artifactory']['ajp']['backlog'] = 100
