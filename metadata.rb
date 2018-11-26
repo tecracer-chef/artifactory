f1127fcaf223b91d9e83c0545b9a73e20c5458c3
Rick Hornsby <rhornsby@ktzr.net>
2018-11-26 13:58:48 -0600

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'info@agileorbit.com'
 license          'Apache V2'
 description      'Installs/Configures artifactory'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.4'
+version          '0.3.5'
 
 recipe 'artifactory::default', 'Installs Artifactory.'
 recipe 'artifactory::apache-proxy', 'Setup Apache reverse proxy in front of Artifactory.'
