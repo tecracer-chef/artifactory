2f6fd62b2548c711b6802c60556d880a4b48ca4a
Patrick Schaumburg <info@p-schaumburg.de>
2019-02-25 09:37:47 +0100

    Update config.rb

--- .travis/config.rb
+++ .travis/config.rb
@@ -1,6 +1,6 @@
 log_level                :info
 log_location             STDOUT
-node_name                'erichelgeson'
+node_name                'pschaumburg'
 client_key               "#{ENV['PWD']}/.travis/publish-key.pem"
 validation_client_name   'chef-validator'
 validation_key           "#{ENV['PWD']}/.travis/publish-key.pem"
