f0f28e57a41053452e3836d151ced4d468e21a95
Patrick Schaumburg <pschaumburg@tecracer.de>
2019-03-17 12:37:50 +0100

    add service test for runit_service, which is used in zip archives

--- test/integration/default/service.rb
+++ test/integration/default/service.rb
@@ -1,5 +1,13 @@
-describe service('artifactory') do
-  it { should be_enabled }
-  it { should be_installed }
-  it { should be_running }
+describe.one do
+  describe service('artifactory') do
+    it { should be_enabled }
+    it { should be_installed }
+    it { should be_running }
+  end
+
+  describe runit_service('artifactory') do
+    it { should be_enabled }
+    it { should be_installed }
+    it { should be_running }
+  end
 end
