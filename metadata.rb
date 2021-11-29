8dfbc66ff77afa6b6c3ab407fc8b1427511285c4
Patrick Schaumburg <pschaumburg@tecracer.de>
2021-11-29 07:20:34 +0100

    update version to 2.0.1 (#34)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'info@p-schaumburg.de'
 license          'Apache-2.0'
 description      'Installs/Configures artifactory'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.0'
+version          '2.0.1'
 
 source_url 'https://github.com/pschaumburg/artifactory' if defined?(:source_url)
 issues_url 'https://github.com/pschaumburg/artifactory/issues' if defined?(:issues_url)
